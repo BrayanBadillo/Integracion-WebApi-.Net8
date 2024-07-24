cfa1f24cf7ae164514b332e97f9f37584c1c003c
devBrayanB <bbadillo@arkeero.com>
2024-07-24 08:42:43 -0500

    Proyecto inicial

---
+++ Data/ProductoData.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Models.Entidades;
+
+namespace Data
+{
+    public static class ProductoData
+    {
+        public static List<Producto> GetProductos = new List<Producto>{
+            new(){ Id = 1, Nombre = "Producto 1", Categoria = "Computadoras", Marca = "Hp", Precio = 2000},
+            new(){ Id = 2, Nombre = "Producto 2", Categoria = "Computadoras", Marca = "Windows", Precio = 3000},
+            new(){ Id = 3, Nombre = "Producto 3", Categoria = "Computadoras", Marca = "Hp", Precio = 4000},
+            new(){ Id = 4, Nombre = "Producto 4", Categoria = "Computadoras", Marca = "Hp", Precio = 5000}
+        };
+    }
+}
