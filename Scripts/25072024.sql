3476338d79f959a3fd31572aa10437b813cb093a
BrayanBadillo <ingbryanbadillo@gmail.com>
2024-07-25 08:18:09 -0500

    Cambios en el Script 22072024.sql

--- Scripts/25072024.sql
+++ Scripts/25072024.sql
@@ -1,51 +1,68 @@
+-- Creacion Base de Datos
+
 CREATE DATABASE IntegracionDB;
 
-USE  IntegracionDB;
+-- Usando Base de Datos
+USE IntegracionDB;
 
-CREATE TABLE Categoria(
-CategoriaId int NOT NULL IDENTITY,
-Nombre NVARCHAR(120) NOT NULL,
-);
+-- Creando Tabla Categoria
 
-ALTER TABLE Categoria
-ADD CONSTRAINT PK_Categoria PRIMARY KEY(CategoriaId);
+CREATE TABLE Categoria(CategoriaId int NOT NULL IDENTITY,
+                                                Nombre NVARCHAR(120) NOT NULL,);
 
-CREATE TABLE Marca(
-MarcaId int NOT NULL IDENTITY,
-Nombre NVARCHAR(120) NOT NULL,
-);
+-- Relacionando Clave Primaria
 
-ALTER TABLE Marca
-ADD CONSTRAINT PK_Marca PRIMARY KEY(MarcaId);
+ALTER TABLE Categoria ADD CONSTRAINT PK_Categoria PRIMARY KEY(CategoriaId);
 
-CREATE TABLE Producto(
-ProductoId int NOT NULL IDENTITY,
-NombreProducto NVARCHAR(120) NOT NULL,
-Precio NUMERIC(7,2) NOT NULL,
-Costo NUMERIC(7,2) NOT NULL,
-CategoriaId INT NOT NULL,
-MarcaId INT NOT NULL
-);
+-- Creando Tabla Marca
 
-ALTER TABLE Producto
-ADD CONSTRAINT PK_Producto PRIMARY KEY(ProductoId);
+CREATE TABLE Marca(MarcaId int NOT NULL IDENTITY,
+                                        Nombre NVARCHAR(120) NOT NULL,);
 
-ALTER TABLE Producto
-ADD CONSTRAINT FK_Producto_Categoria_CategoriaId FOREIGN KEY(CategoriaId)
-REFERENCES Categoria(CategoriaId) ON DELETE CASCADE;
+-- Relacionando Clave Primaria
 
-ALTER TABLE Producto
-ADD CONSTRAINT FK_Producto_Marca_MarcaId FOREIGN KEY(MarcaId)
-REFERENCES Marca(MarcaId) ON DELETE CASCADE;
+ALTER TABLE Marca ADD CONSTRAINT PK_Marca PRIMARY KEY(MarcaId);
+
+-- Creando Tabla Producto
+
+CREATE TABLE Producto(ProductoId int NOT NULL IDENTITY,
+                                              NombreProducto NVARCHAR(120) NOT NULL,
+                                                                           Precio NUMERIC(7,2) NOT NULL,
+                                                                                               Costo NUMERIC(7,2) NOT NULL,
+                                                                                                                  CategoriaId INT NOT NULL,
+                                                                                                                                  MarcaId INT NOT NULL);
+
+-- Relacionando Clave Primaria
+
+ALTER TABLE Producto ADD CONSTRAINT PK_Producto PRIMARY KEY(ProductoId);
+
+-- Relacionando Clave Foranea con Categoria
+
+ALTER TABLE Producto ADD CONSTRAINT FK_Producto_Categoria_CategoriaId
+FOREIGN KEY(CategoriaId) REFERENCES Categoria(CategoriaId) ON
+DELETE CASCADE;
+
+-- Relacionando Clave Foranea con Producto
+
+ALTER TABLE Producto ADD CONSTRAINT FK_Producto_Marca_MarcaId
+FOREIGN KEY(MarcaId) REFERENCES Marca(MarcaId) ON
+DELETE CASCADE;
+
+-- Inserccion de datos en tabla Categoria
 
 INSERT INTO Categoria(Nombre)
 VALUES('Computadoras');
 
+
 INSERT INTO Categoria(Nombre)
 VALUES('Impresoras');
+
+-- Inserccion de datos en tabla Marca
 
 INSERT INTO Marca(Nombre)
 VALUES('HP');
 
+
 INSERT INTO Marca(Nombre)
 VALUES('Apple');
+
