131f3371a3438c0a846a2e3d987f3bc916745cd3
BrayanBadillo <ingbryanbadillo@gmail.com>
2024-07-26 13:07:43 -0500

    Agregado script de insert de 2 productos en la tabla producto

---
+++ Scripts/26072024.sql
@@ -0,0 +1,14 @@
+INSERT INTO Producto(NombreProducto, CategoriaId, MarcaId, Precio, Costo)
+VALUES('Hp - Envy',
+       1,
+       1,
+       2000,
+       1500);
+
+
+INSERT INTO Producto(NombreProducto, CategoriaId, MarcaId, Precio, Costo)
+VALUES('Hp - EliteBook',
+       1,
+       1,
+       2500,
+       1800)
